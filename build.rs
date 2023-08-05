81faecace35613fa7604de470e3d3cfc7c4b4f7a
Joshua Patterson <joshua.l.patterson7@gmail.com>
2023-08-05 18:08:50 -0500

    Switched to iced for GUI

--- build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    slint_build::compile("src/ui/hello.slint").unwrap();
-}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,51 @@
-slint::include_modules!();
-fn main() {
-    HelloWorld::new().unwrap().run().unwrap();
+use iced::widget::{button, row, text};
+use iced::{Alignment, Element, Padding, Sandbox, Settings};
+
+
+fn main()  -> iced::Result {
+    Counter::run(Settings::default())
+}
+
+struct Counter {
+    value: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self { value: 0 }
+    }
+
+    fn title(&self) -> String {
+        String::from("Counter - Iced")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        row![
+            button("Increment").on_press(Message::IncrementPressed),
+            text(self.value).size(50),
+            button("Decrement").on_press(Message::DecrementPressed)
+        ]
+        .padding(Padding::from([20, 0, 0, 20]))
+        .align_items(Alignment::Center)
+        .into()
+    }
 }

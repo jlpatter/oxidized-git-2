bfb3757141bc5d0bdb50de0bcddc68d879c62af1
Joshua Patterson <joshua.l.patterson7@gmail.com>
2023-08-19 22:15:52 -0500

    Switch to egui and add default open directory

--- src/git_functions.rs
+++ src/git_functions.rs
@@ -1,10 +1,16 @@
+use std::path::PathBuf;
 use anyhow::{bail, Result};
+use directories::UserDirs;
 use git2::Repository;
 use rfd::FileDialog;
 
 pub fn open_repo() -> Result<Option<Repository>> {
+    let start_dir = match UserDirs::new() {
+        Some(ud) => PathBuf::from(ud.home_dir()),
+        None => PathBuf::from("/"),
+    };
     let folder = FileDialog::new()
-        .set_directory("/")
+        .set_directory(start_dir)
         .pick_folder();
     if let Some(pf) = folder {
         match Repository::open(pf) {
--- src/main.rs
+++ src/main.rs
@@ -2,71 +2,43 @@ mod git_functions;
 
 use anyhow::Result;
 use git2::Repository;
-use iced::widget::{button, Column, Row};
-use iced::{Alignment, Element, Padding, Sandbox, Settings};
 
-
-fn main()  -> iced::Result {
-    OxidizedGit2::run(Settings::default())
-}
-
-struct OxidizedGit2 {
-    repo: Option<Repository>
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    OpenPressed,
-    FetchPressed,
-}
-
-impl OxidizedGit2 {
-    fn handle_error<T>(result: Result<T>) -> Option<T> {
-        match result {
-            Ok(t) => Some(t),
-            Err(e) => {
-                // TODO: Handle errors in some way!
-                None
-            },
-        }
+fn handle_error<T>(result: Result<T>) -> Option<T> {
+    match result {
+        Ok(t) => Some(t),
+        Err(_e) => {
+            // TODO: Handle errors in some way!
+            None
+        },
     }
 }
 
-impl Sandbox for OxidizedGit2 {
-    type Message = Message;
+fn main()  -> eframe::Result<()> {
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(840.0, 680.0)),
+        ..Default::default()
+    };
 
-    fn new() -> Self {
-        Self {
-            repo: None,
-        }
-    }
+    // Our application state (put persistent stuff here):
+    let mut repo: Option<Repository> = None;
 
-    fn title(&self) -> String {
-        String::from("Oxidized Git 2")
-    }
-
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::OpenPressed => {
-                let repo_opt_opt = OxidizedGit2::handle_error(git_functions::open_repo());
-                if let Some(repo_opt) = repo_opt_opt {
-                    self.repo = repo_opt;
+    eframe::run_simple_native("Oxidized Git 2", options, move |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                if ui.button("Open").clicked() {
+                    let repo_opt_opt = handle_error(git_functions::open_repo());
+                    if let Some(repo_opt) = repo_opt_opt {
+                        repo = repo_opt;
+                    }
                 }
-            }
-            Message::FetchPressed => {
-                // TODO: Implement Fetch.
-            }
-        }
-    }
-
-    fn view(&self) -> Element<Message> {
-        let btn_row: Row<Message> = Row::new()
-            .push(button("Open").on_press(Message::OpenPressed))
-            .push(button("Fetch").on_press(Message::FetchPressed));
-
-        Column::new().push(btn_row)
-            .padding(Padding::from([20, 0, 0, 20]))
-            .align_items(Alignment::Center)
-            .into()
-    }
+                if ui.button("Fetch").clicked() {
+                    // TODO: Implement Fetch
+                    match &repo {
+                        Some(r) => println!("{:?}", r.path()),
+                        None => println!("None"),
+                    };
+                }
+            });
+        });
+    })
 }

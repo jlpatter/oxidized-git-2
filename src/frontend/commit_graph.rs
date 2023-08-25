f1ec5630ee8ba7e7659eb1d149e36f987bb8adec
Joshua Patterson <joshua.l.patterson7@gmail.com>
2023-08-25 13:17:02 -0500

    Set graph commits to have x and y values

--- src/frontend/commit_graph.rs
+++ src/frontend/commit_graph.rs
@@ -4,6 +4,7 @@ use git2::{Oid, Repository};
 use crate::backend::git_functions::git_revwalk;
 
 const X_OFFSET: f32 = 10.0;
+const X_SPACING: f32 = 15.0;
 const Y_OFFSET: f32 = 10.0;
 const Y_SPACING: f32 = 30.0;
 const CIRCLE_RADIUS: f32 = 7.0;
@@ -11,19 +12,24 @@ const CIRCLE_RADIUS: f32 = 7.0;
 const GRAPH_COLORS: [Color32; 4] = [Color32::BLUE, Color32::GREEN, Color32::YELLOW, Color32::RED];
 
 struct Commit {
+    // NOTE: X and Y here are not pixel coordinates, they act more like indexes of valid 'positions'.
+    x: usize,
+    y: usize,
     summary: String,
 }
 
 impl Commit {
-    pub fn new(repo: &Repository, oid: Oid) -> Result<Self> {
+    pub fn new(repo: &Repository, i: usize, oid: Oid) -> Result<Self> {
         let commit = repo.find_commit(oid)?;
         Ok(Self {
+            x: 0,
+            y: i,
             summary: String::from(commit.summary().ok_or(Error::msg("Commit summary has invalid UTF-8!"))?),
         })
     }
 
-    pub fn show(&self, painter: &Painter, index: usize, start_position: Pos2) {
-        let circle_position = start_position + Vec2::new(X_OFFSET, Y_OFFSET + Y_SPACING * index as f32);
+    pub fn show(&self, painter: &Painter, start_position: Pos2) {
+        let circle_position = start_position + Vec2::new(X_OFFSET + X_SPACING * self.x as f32, Y_OFFSET + Y_SPACING * self.y as f32);
         painter.circle_filled(circle_position, CIRCLE_RADIUS, GRAPH_COLORS[0]);
         painter.text(circle_position + Vec2::new(X_OFFSET, 0.0), Align2::LEFT_CENTER, self.summary.clone(), FontId::default(), Color32::WHITE);
     }
@@ -35,14 +41,18 @@ pub struct CommitGraph {
 
 impl CommitGraph {
     pub fn new(repo: &Repository) -> Result<Self> {
+        Ok(Self {
+            commits: CommitGraph::get_commits(repo)?,
+        })
+    }
+
+    fn get_commits(repo: &Repository) -> Result<Vec<Commit>> {
         let oid_vec = git_revwalk(repo)?;
         let mut commits = vec![];
-        for oid in oid_vec {
-            commits.push(Commit::new(repo, oid)?);
+        for (i, oid) in oid_vec.iter().enumerate() {
+            commits.push(Commit::new(repo, i, *oid)?);
         }
-        Ok(Self {
-            commits,
-        })
+        Ok(commits)
     }
 
     pub fn show(&mut self, ui: &mut Ui) {
@@ -52,8 +62,8 @@ impl CommitGraph {
                 let (response, painter) = ui.allocate_painter(Vec2::new(ui.available_width(), scroll_area_height), Sense::hover());
                 let start_position = response.rect.left_top();
 
-                for (i, commit) in self.commits.iter().enumerate() {
-                    commit.show(&painter, i, start_position);
+                for commit in &self.commits {
+                    commit.show(&painter, start_position);
                 }
             });
         });

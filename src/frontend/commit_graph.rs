821c1342bcf881ee97800b75a480e9aff5adcafe
Joshua Patterson <jpatterson@canopyteam.org>
2023-09-01 10:44:13 -0500

    WIP 3

--- src/frontend/commit_graph.rs
+++ src/frontend/commit_graph.rs
@@ -15,6 +15,7 @@ const LINE_STROKE_WIDTH: f32 = 3.0;
 const GRAPH_COLORS: [Color32; 4] = [Color32::BLUE, Color32::GREEN, Color32::YELLOW, Color32::RED];
 const VISIBLE_SCROLL_AREA_PADDING: usize = 10;
 
+#[derive(Copy, Clone)]
 struct LocationIndex {
     x: usize,
     y: usize,
@@ -45,96 +46,63 @@ impl LocationIndex {
     }
 }
 
-enum GraphElement {
-    Circle,
-    Summary,
-    Line(Line),
-}
-
 struct GraphRow {
     // NOTE: X and Y here are not pixel coordinates, they act more like indexes of valid 'positions'.
-    oid: Oid,
+    circle_location: Rc<RefCell<LocationIndex>>,
+    summary_location: LocationIndex,
     summary: String,
-    elements: Vec<GraphElement>,
+    lines: Vec<Line>,
 }
 
 impl GraphRow {
-    pub fn new(commit: git2::Commit, i: usize) -> Result<Self> {
-        Ok(Self {
-            oid: commit.id(),
-            summary: String::from(commit.summary().ok_or(Error::msg("Commit summary has invalid UTF-8!"))?),
-            elements: vec![],
-        })
-    }
-
-    fn get_pixel_x(x: usize) -> f32 {
-        X_OFFSET + X_SPACING * x as f32
-    }
-
-    fn get_pixel_y(y: usize) -> f32 {
-        Y_OFFSET + Y_SPACING * y as f32
-    }
-
-    fn get_relative_pos2(x: usize, y: usize, scroll_area_top_left: Pos2) -> Pos2 {
-        scroll_area_top_left + Vec2::new(GraphRow::get_pixel_x(x), GraphRow::get_pixel_y(y))
-    }
-
-    fn get_color(x: usize) -> Color32 {
-        GRAPH_COLORS[x % GRAPH_COLORS.len()]
-    }
-
-    pub fn show(&self, painter: &Painter, y: usize, scroll_area_top_left: Pos2) {
-        for (x, element) in self.elements.iter().enumerate() {
-            match element {
-                GraphElement::Circle => {
-                    painter.circle_filled(
-                        GraphRow::get_relative_pos2(x, y, scroll_area_top_left),
-                        CIRCLE_RADIUS,
-                        GraphRow::get_color(x)
-                    );
-                }
-                GraphElement::Summary => {
-                    painter.text(
-                        GraphRow::get_relative_pos2(x, y, scroll_area_top_left),
-                        Align2::LEFT_CENTER,
-                        self.summary.clone(),
-                        FontId::default(),
-                        Color32::WHITE
-                    );
-                }
-                GraphElement::Line(line) => {
-                    line.show(painter, scroll_area_top_left);
-                }
-            };
-        }
-    }
-}
-
-struct Line {
-    end_element: Rc<RefCell<GraphElement>>,
-}
-
-impl Line {
-    pub fn new(start_x: usize, start_y: usize, end_x: usize, end_y: usize) -> Self {
-        let start = LocationIndex::new(start_x, start_y);
-        let end = LocationIndex::new(end_x, end_y);
-        let color;
-        if start_x < end_x {
-            color = end.get_color();
-        } else {
-            color = start.get_color();
-        }
+    pub fn new(circle_location: Rc<RefCell<LocationIndex>>, summary_location: LocationIndex, summary: String) -> Self {
         Self {
-            start,
-            end,
-            color,
+            circle_location,
+            summary_location,
+            summary,
+            lines: vec![],
         }
     }
 
     pub fn show(&self, painter: &Painter, scroll_area_top_left: Pos2) {
+        for line in &self.lines {
+            line.show(painter, scroll_area_top_left);
+        }
+        let circle_location = self.circle_location.borrow();
+        painter.circle_filled(
+            circle_location.get_relative_pos2(scroll_area_top_left),
+            CIRCLE_RADIUS,
+            circle_location.get_color()
+        );
+        painter.text(
+            self.summary_location.get_relative_pos2(scroll_area_top_left),
+            Align2::LEFT_CENTER,
+            self.summary.clone(),
+            FontId::default(),
+            Color32::WHITE
+        );
+    }
+}
+
+struct Line {
+    start_location: Rc<RefCell<LocationIndex>>,
+    end_location: Rc<RefCell<LocationIndex>>,
+}
+
+impl Line {
+    pub fn new(start_location: Rc<RefCell<LocationIndex>>, end_location: Rc<RefCell<LocationIndex>>) -> Self {
+        Self {
+            start_location,
+            end_location,
+        }
+    }
+
+    pub fn show(&self, painter: &Painter, scroll_area_top_left: Pos2) {
+        let start_location = self.start_location.borrow();
+        let end_location = self.end_location.borrow();
         painter.line_segment(
-            [self.start.get_relative_pos2(scroll_area_top_left), self.end.get_relative_pos2(scroll_area_top_left)],
-            Stroke::new(LINE_STROKE_WIDTH, self.color)
+            [start_location.get_relative_pos2(scroll_area_top_left), end_location.get_relative_pos2(scroll_area_top_left)],
+            Stroke::new(LINE_STROKE_WIDTH, start_location.get_color())
         );
     }
 }
@@ -155,10 +123,26 @@ impl CommitGraph {
         // Loop through once to get all the commits and create a mapping to get the parents later.
         let revwalk = git_revwalk(repo)?;
         let mut graph_rows = vec![];
+        let mut graph_row_commit_map: HashMap<Oid, Rc<RefCell<GraphRow>>> = HashMap::new();
         for (i, oid_result) in revwalk.enumerate() {
             let oid = oid_result?;
             let git_commit = repo.find_commit(oid)?;
-            let graph_row_rc = Rc::new(RefCell::new(GraphRow::new(git_commit, i)?));
+
+            let summary = String::from(git_commit.summary().ok_or(Error::msg("Commit message has invalid UTF-8!"))?);
+            let graph_row_rc = Rc::new(RefCell::new(GraphRow::new(
+                Rc::new(RefCell::new(LocationIndex::new(0, i))),
+                LocationIndex::new(1, i),
+                summary,
+            )));
+
+            for parent_oid in git_commit.parent_ids() {
+                if let Some(parent_row_rc) = graph_row_commit_map.get(&parent_oid) {
+                    let parent_row = parent_row_rc.borrow();
+                    // TODO: Connect the parents to the current row here!
+                }
+            }
+
+            graph_row_commit_map.insert(oid, graph_row_rc.clone());
             graph_rows.push(graph_row_rc);
         }
         Ok(graph_rows)
@@ -180,7 +164,7 @@ impl CommitGraph {
                 let visible_area_bottom_index = (((scroll_position + visible_area_height - Y_OFFSET) / Y_SPACING) as usize + VISIBLE_SCROLL_AREA_PADDING).min(self.graph_rows.len());
 
                 for i in visible_area_top_index..visible_area_bottom_index {
-                    self.graph_rows[i].borrow().show(&painter, i, scroll_area_top_left);
+                    self.graph_rows[i].borrow().show(&painter, scroll_area_top_left);
                 }
             });
         });

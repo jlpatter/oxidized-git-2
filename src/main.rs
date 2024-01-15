9858e3ebccb9b88f418560bae235aa1bbc5e4cbb
Joshua Patterson <joshua.l.patterson7@gmail.com>
2024-01-15 15:16:06 -0600

    Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use egui::ViewportBuilder;
 use crate::frontend::app::OG2App;
 
 mod frontend;
@@ -6,9 +7,8 @@ mod backend;
 fn main()  -> eframe::Result<()> {
     let options = eframe::NativeOptions {
         // This is broken for some reason :(
-        maximized: true,
+        viewport: ViewportBuilder::default().with_app_id("oxidized-git-2".to_owned()).with_maximized(true),
         centered: true,
-        app_id: Some("oxidized-git-2".to_owned()),
         ..Default::default()
     };
     eframe::run_native("Oxidized Git 2", options, Box::new(|cc| Box::new(OG2App::new(cc))))
